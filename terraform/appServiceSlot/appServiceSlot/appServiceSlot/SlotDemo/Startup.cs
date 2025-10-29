971b69e48741afaa06a4340f390104d2b1848dd7
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-10-29 15:05:44 +0530

    Adddeploy

--- terraform/appServiceSlot/appServiceSlot/appServiceSlot/SlotDemo/Startup.cs
+++ terraform/appServiceSlot/appServiceSlot/appServiceSlot/SlotDemo/Startup.cs
@@ -25,7 +25,7 @@ namespace SlotDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
