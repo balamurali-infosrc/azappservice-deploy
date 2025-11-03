1bb015c7cf9c3ad2bc0f804e99f2d947b8e3069d
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-11-03 12:31:02 +0530

    AppservicedeployApplication

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,3 @@
-output "app_service_url" {
-  value = azurerm_linux_web_app.webapp.default_hostname
-}
-
-output "staging_slot_url" {
-  value = azurerm_linux_web_app_slot.staging.default_hostname
-}
 output "webapp_name" {
   value = azurerm_linux_web_app.webapp.name
 }
@@ -16,3 +9,11 @@ output "slot_name" {
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+output "app_service_url" {
+  value = azurerm_linux_web_app.webapp.default_hostname
+}
+
+output "staging_slot_url" {
+  value = azurerm_linux_web_app_slot.staging.default_hostname
+}
+
